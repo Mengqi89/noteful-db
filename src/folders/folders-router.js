0de7b5d0f7318b264b961bf23e1b4e094c90b639
Julie Broderick <broderick.julie@gmail.com>
2019-05-27 22:09:38 -0500

    making sure folders router is loading

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -8,13 +8,16 @@ const jsonParser = express.json();
 
 foldersRouter
   .route('/')
+  // .get((req, res, next) => {
+  //   const knexInstance = req.app.get('db');
+  //   FoldersService.getAllFolders(knexInstance)
+  //     .then(folders => {
+  //       res.json(folders);
+  //     })
+  //     .catch(next);
+  // })
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db');
-    FoldersService.getAllFolders(knexInstance)
-      .then(folders => {
-        res.json(folders);
-      })
-      .catch(next);
+    res.send('getting all folders')
   })
   .post(jsonParser, (req, res, next) => {
     const knexInstance = req.app.get('db');
